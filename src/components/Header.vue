53cc49d3872ea2691072a39bff4f7ce9dfc52ccd
Jack <jack.branthwaite@gmail.com>
2021-06-25 19:47:45 +1200

    Routing implemented properly - basic application finished

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
   <header>
     <h1>{{ title }}</h1>
     <Button
+      v-show="homePage"
       @btn-click="$emit('toggle-add-task')"
       :text="showAddTask ? 'Close' : 'Add Task'"
       :color="showAddTask ? 'red' : 'green'"
@@ -17,7 +18,18 @@ export default {
     title: String,
     showAddTask: Boolean,
   },
-  components: { Button },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
   emits: ["toggle-add-task"],
 };
 </script>
